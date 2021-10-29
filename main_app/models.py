5e53d34c7be291320a853d86ae11cb2caf3b61b7
alondra143 <not given>
2021-10-29 13:33:01 -0700

    complete step 3.1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Widget(models.Model):
+    description = models.TextField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.description
+        
+    def get_absolute_url(self):
+        return reverse('home', kwargs={'widget_id': self.id})
