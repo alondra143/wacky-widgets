5ebdaa9fa7c37ca1fba1422aa50cd199ba42e2ad
alondra143 <not given>
2021-10-29 13:42:40 -0700

    create widget model and deletewidgetform

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Widget
-from .forms import WidgetForm
+from .forms import DeleteWidgetForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html');
+
+def add_widget(request):
+    #make the ModelForm from the request.POST
+    pass
+    #save to database
+    #redirect to home
+
+def delete_widget(request, widget_id):
+    #get the widget that is being deleted by its pk and delete
+    #redirect to home
+    pass
